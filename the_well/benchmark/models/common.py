fb8b42ef1db9a4100ed41a91e8c776a5df4be1ac
AnnihilatorChess <150355206+AnnihilatorChess@users.noreply.github.com>
2025-11-18 21:05:12 +0100

    FiLM naive setup

--- the_well/benchmark/models/common.py
+++ the_well/benchmark/models/common.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -60,3 +61,75 @@ class SN_MLP(nn.Module):
 
     def forward(self, x):
         return self.fc2(self.act(self.fc1(x)))
+
+###################
+# here are some helper classes for FiLM
+###################
+
+class FourierEncoding(nn.Module):
+    """Fourier encoding: NeRF style, cite Mildenhall et al. (2020)."""
+    def __init__(self, num_bands, include_input=False):
+        super().__init__()
+        self.num_bands = num_bands
+        self.include_input = include_input
+        self.freq_bands = 2 ** torch.linspace(0, num_bands-1, num_bands)
+
+    def forward(self, x):
+        # expects input: (B, 1)
+        enc = [] if not self.include_input else [x]
+
+        for f in self.freq_bands:
+            enc.append(torch.sin(math.pi * f * x))
+            enc.append(torch.cos(math.pi * f * x))
+        return torch.cat(enc, dim=1)
+
+
+class EmbedFeatures(nn.Module):
+    """Uses Sinusoidal Fourier encoding and MLP to embed a feature scalar."""
+    def __init__(self, hidden_dim, num_bands, include_input=False):
+        super().__init__()
+        self.FourierEncoding = FourierEncoding(num_bands, include_input)
+        self.MLP = MLP(hidden_dim)
+
+    def forward(self, inputs):
+        encodings = [self.FourierEncoding(x) for x in inputs]
+        encodings = torch.cat(encodings, dim=1)
+        return self.MLP(encodings)
+
+class FiLMLayer(nn.Module):
+    """This generates FiLM Layers. It returns gamma and beta from conditioning parameters"""
+    def __init__(self, t_cool_embed_dim, time_embed_dim, feature_channels, hidden_dim):
+        super().__init__()
+
+        # embed time and t_cool via MLP
+        self.time_embedding = nn.Sequential(
+            nn.Linear(1, time_embed_dim),
+            nn.GELU(),
+            nn.Linear(time_embed_dim, time_embed_dim),
+        )
+        self.t_cool_embedding = nn.Sequential(
+            nn.Linear(1, t_cool_embed_dim),
+            nn.GELU(),
+            nn.Linear(t_cool_embed_dim, t_cool_embed_dim),
+        )
+        # MLP for generating beta and gamma
+        self.generator = nn.Sequential(
+            nn.Linear(time_embed_dim + t_cool_embed_dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, 2*feature_channels),
+        )
+        # Initialize gamma to 1 and beta to 0
+        nn.init.constant_(self.generator.bias[feature_channels:], 0)
+        nn.init.constant_(self.generator.bias[:feature_channels], 1)
+
+    def forward(self, t_cool, time):
+        # embed conditioning params
+        embedded_time = self.time_embedding(time)
+        embedded_t_cool = self.t_cool_embedding(t_cool)
+        conditioning_input = torch.cat([embedded_time, embedded_t_cool], dim=1)
+        # convert to gamma beta
+        gammas_betas = self.generator(conditioning_input)
+        gamma, beta = torch.chunk(gammas_betas, 2, dim=1)
+        # reshape: [B, C] --> [B, C, 1, 1]
+        gamma, beta = gamma.unsqueeze(-1).unsqueeze(-1), beta.unsqueeze(-1).unsqueeze(-1)
+        return gamma, beta
