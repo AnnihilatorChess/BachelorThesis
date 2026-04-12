Fixed metrics calculation for 1d, added some scripts

the_well/benchmark/metrics/plottable_data.py
@@ -93,7 +93,9 @@ def build_1d_power_spectrum(x, spatial_dims):
     x_fft = torch.fft.fftn(x, dim=spatial_dims, norm="ortho").abs().square()
     # Return the shifted sqrt power spectrum
     # First average over spatial dims, then take the last time step from the first batch element
-    return torch.fft.fftshift(x_fft.mean(spatial_dims[1:])[0, -1].sqrt())
+    if len(spatial_dims) > 1:
+        x_fft = x_fft.mean(spatial_dims[1:])
+    return torch.fft.fftshift(x_fft[0, -1].sqrt())
 
 
 def plot_power_spectrum_by_field(

the_well/benchmark/metrics/spectral.py
@@ -20,7 +20,10 @@ def fftn(x: torch.Tensor, meta: WellMetadata) -> torch.Tensor:
         N-dimensional FFT of x.
     """
     spatial_dims = tuple(range(-meta.n_spatial_dims - 1, -1))
-    return torch.fft.fftn(x, dim=spatial_dims)
+    if x.is_cuda:
+        with torch.cuda.device(x.device):
+            return torch.fft.fftn(x.contiguous(), dim=spatial_dims)
+    return torch.fft.fftn(x.contiguous(), dim=spatial_dims)
 
 
 def ifftn(x: torch.Tensor, meta: WellMetadata) -> torch.Tensor:
@@ -35,7 +38,10 @@ def ifftn(x: torch.Tensor, meta: WellMetadata) -> torch.Tensor:
         N-dimensional inverse FFT of x.
     """
     spatial_dims = tuple(range(-meta.n_spatial_dims - 1, -1))
-    return torch.fft.ifftn(x, dim=spatial_dims)
+    if x.is_cuda:
+        with torch.cuda.device(x.device):
+            return torch.fft.ifftn(x.contiguous(), dim=spatial_dims)
+    return torch.fft.ifftn(x.contiguous(), dim=spatial_dims)
 
 
 def power_spectrum(